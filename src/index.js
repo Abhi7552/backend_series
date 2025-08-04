7f878e552d3cde4cd08a142173a0922b902e230e
Abhi7552 <123100483+Abhi7552@users.noreply.github.com>
2025-08-04 17:14:04 +0530

    Mongodb Atlas connection successfull !!

---
+++ src/index.js
@@ -0,0 +1,33 @@
+// require('dotenv').config({path:'./env'})
+
+// import dotenv from "dotenv";
+// dotenv.config({
+//     path:'./env'
+// })
+
+import 'dotenv/config'
+import express from "express";
+import connectDB from "./db/index.js";
+
+connectDB();
+
+
+
+
+// ;(async ()=>{})()  better approach to handle IIFE 
+/*const app=express();
+(async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        app.on('error',(err)=>{
+            console.log("Error in DB :",err);
+            throw err;
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`App is listening on port : ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.error("DB Connection Error :",error)
+    }
+})()*/

2ebf028fbb0da6cba85a9540f97896347bdbf19f
Abhi7552 <123100483+Abhi7552@users.noreply.github.com>
2025-08-16 20:53:41 +0530

    added routes for postman

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {registerUser,loginUser,logOutUser,refreshAccessToken} from "../controllers/user.controller.js";
+import { registerUser, loginUser, logOutUser, refreshAccessToken, changeCurrentPassword, getCurrentuser, updateAccountDetails, avtarUpdate, coverImageUpdate, getUserChannelProfile, getWatchHistory } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 
@@ -8,13 +8,13 @@ const router = Router();
 
 router.route("/register").post(
     upload.fields([
-        { 
-            name: "avtar" ,
-            maxCount:1
+        {
+            name: "avtar",
+            maxCount: 1
         },
-        { 
+        {
             name: "coverImage",
-            maxCount:1
+            maxCount: 1
         }
     ]),
     registerUser
@@ -24,7 +24,16 @@ router.route("/login").post(loginUser);
 
 // secured routes
 
-router.route("/logout").post(verifyJwt,logOutUser);
+router.route("/logout").post(verifyJwt, logOutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJwt, changeCurrentPassword);
+router.route("/current-user").get(verifyJwt, getCurrentuser);
+router.route("/update-account").patch(verifyJwt, updateAccountDetails);
+
+router.route("/update-avtar").patch(verifyJwt, upload.single("avtar"), avtarUpdate);
+router.route("/update-coverImage").patch(verifyJwt, upload.single("coverImage"), coverImageUpdate);
+
+router.route("/c/:username").get(verifyJwt, getUserChannelProfile);
+router.route("/history").get(verifyJwt, getWatchHistory);
 
 export default router;
